4886d564acfc8be55109b77a58a13acc034707cd
Eddan <edvin.runhellen1@gmail.com>
2025-01-07 20:31:33 +0100

    Added request: get all products

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express';
 const app = express();
 
+//middlewares
+app.use(express.json())
+
+
+app.get('/products', function (req, res) {
+    res.send(products)
+})
+
 const products = [
     {
         id: "1",
