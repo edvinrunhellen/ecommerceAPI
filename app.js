fe2307d9d7a4895880bfa6e3ae5e7685c655fdcf
Eddan <edvin.runhellen1@gmail.com>
2025-01-07 20:51:24 +0100

    Added post, put, delete and limit requests

--- app.js
+++ app.js
@@ -5,9 +5,36 @@ const app = express();
 app.use(express.json())
 
 
-app.get('/products', function (req, res) {
-    res.send(products)
+app.post('/products', function (req, res) {
+    res.send("product added to inventory")
 })
+
+app.put('/products/:productId', function (req, res) {
+    const productId = req.params.productId
+    res.send("product updated in inventory")
+})
+
+app.get('/products/:productId', function (req, res) {
+    const productId = req.params.productId
+    const product = products.find(p => p.id === productId); // Sök efter produkten
+    if (product) {
+        res.send(product); // Om produkten hittas, returnera den
+    } else {
+        res.status(404).send({ message: "Product not found" }); // Om inte, returnera 404
+    }
+})
+
+app.delete('/products/:productId', function (req, res) {
+    const productId = req.params.productId
+    res.send("product deleted in inventory")
+})
+
+app.get('/products', function (req, res) {
+    const limit = parseInt(req.query.limit) || products.length;
+    const limitedProducts = products.slice(0, limit);
+    res.send(limitedProducts);
+});
+
 
 const products = [
     {
